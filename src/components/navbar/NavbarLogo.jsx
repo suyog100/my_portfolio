3d508974095a9870bcacd8a2a818c83b809947a6
suyog100 <skmanandhar803@gmail.com>
2025-03-03 21:16:40 +0545

    navbar issue

--- src/components/navbar/NavbarLogo.jsx
+++ src/components/navbar/NavbarLogo.jsx
@@ -5,7 +5,7 @@ const NavbarLogo = () => {
         Suyog Manandhar
       </h1>
       <h1 className="text-white font-special font-extrabold text-4xl md:hidden sm:block">
-        AQ
+        SM
       </h1>
     </div>
   );
